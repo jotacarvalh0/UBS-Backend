2eeb565e19b4f7316cd81129941dfc99eb8b0efb
jotacarvalh0 <josecarvalho11235@gmail.com>
2024-11-22 15:55:15 -0300

    Incorporando dados Mocados

---
+++ src/data/ubsData.js
@@ -0,0 +1,23 @@
+// src/data/ubsData.js
+const ubsData = [
+    {
+        id: 1,
+        nome: "UBS SÃO MIGUEL", 
+        endereco: "RUA SÃO FRANCISCO - CENTRO",
+        location: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5499.3808625067395!2d-39.31366732500075!3d-7.2045871928009415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a178b2a4a1ad91%3A0xec0e908e05d37d8f!2sUnidade%20B%C3%A1sica%20de%20Sa%C3%BAde%2040%20S%C3%A3o%20Miguel!5e1!3m2!1spt-BR!2sbr!4v1732301104444!5m2!1spt-BR!2sbr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+    },
+    {
+        id: 2,
+        nome: "UBS Jardim Gonzaga", 
+        endereco: "RUA MANOEL MIGUEL DOS SANTOS , S/N - LAGOA SECA - JUAZEIRO DO NORTE",
+        location: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5506.947078602561!2d-39.32590497326468!3d-7.249043542171455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a1829c53c340df%3A0x80f821ecc1435f5!2sUnidade%20B%C3%A1sica%20de%20Sa%C3%BAde%20Lagoa%20Seca!5e1!3m2!1spt-BR!2sbr!4v1732301507092!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+    },
+    {
+        id: 3,
+        nome: "UBS FRANCISCANOS", 
+        endereco: "RUA CAMPOS ELÍSEOS , S/N - FRANCISCANOS - CNES: 5799015 - JUAZEIRO DO NORTE",
+        location: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10998.525171946267!2d-39.318070058697636!3d-7.214328821165578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a178a39112b28d%3A0x5ae735e74b9c992e!2sUnidade%20B%C3%A1sica%20de%20Sa%C3%BAde%20-%20Franciscanos!5e1!3m2!1spt-BR!2sbr!4v1732301676528!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+    },
+];
+
+module.exports = ubsData;
